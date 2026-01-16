5718bb6acdbe72086f43ab059be6fd7a53312d83
Remi Vansteelandt <remvst@gmail.com>
2026-01-15 21:16:53 -0500

    Settings context menu

--- src/background/index.ts
+++ src/background/index.ts
@@ -25,4 +25,17 @@ chrome.action.onClicked.addListener((tab) => {
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.tabs.create({ url: chrome.runtime.getURL("sidepanel/sidepanel.html") });
+
+  chrome.contextMenus.removeAll();
+  chrome.contextMenus.create({
+    id: "power-mode-settings",
+    title: "Power Mode Settings",
+    contexts: ["all"],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "power-mode-settings" && tab?.id) {
+    openSidePanel(tab.id);
+  }
 });
